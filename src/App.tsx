5d3164aa72953b660cd5ad9a66c17601018c640e
samuelhirsch <shmielhirsch@gmail.com>
2026-04-30 16:56:33 -0400

    lets see

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router'
-import Home from './home'
-import Header from './header'
-import ContactUs from './contactUs'
-import AboutUs from './aboutUs'
-import MortgageCalculator from './calculator'
-import Charts from './charts'
+import { Routes, Route, BrowserRouter } from "react-router";
+import Home from "./home";
+import ContactUs from "./contactUs";
+import AboutUs from "./aboutUs";
+import Rates from "./charts";
+import Calculator from "./calculator";
+import Header from "./header";
+
+/** Match Vite base (e.g. "/" or "/FakeMortgageWeb/"); BrowserRouter basename must not end with `/` */
+const routerBasename =
+  import.meta.env.BASE_URL.replace(/\/$/, "") || "/";
+
 function App() {
-    return (
-        <div className="app-root">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Header />}>
-                        <Route index element={<Home />} />
-                        <Route path="/ContactUs" element={<ContactUs />} />
-                        <Route path="/AboutUs" element={<AboutUs />} />
-                        <Route path="/Calculator" element={<MortgageCalculator />} />
-                        <Route path="/Rates" element={<Charts />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-        </div>
-    )
+  return (
+    <BrowserRouter basename={routerBasename}>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="ContactUs" element={<ContactUs />} />
+          <Route path="AboutUs" element={<AboutUs />} />
+          <Route path="Calculator" element={<Calculator />} />
+          <Route path="Rates" element={<Rates />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/nav.tsx
+++ src/nav.tsx
@@ -1,15 +1,27 @@
-import { NavLink } from 'react-router'
+import { NavLink } from "react-router";
 
-export default function Nav() {
+function Nav() {
   return (
-    <nav className="site-nav" aria-label="Primary">
+    <nav className="site-nav" aria-label="Main">
       <ul>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/ContactUs">Contact</NavLink></li>
-        <li><NavLink to="/AboutUs">About</NavLink></li>
-        <li><NavLink to="/Calculator">Calculator</NavLink></li>
-        <li><NavLink to="/Rates">Rates</NavLink></li>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/ContactUs">Contact</NavLink>
+        </li>
+        <li>
+          <NavLink to="/AboutUs">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/Calculator">Calculator</NavLink>
+        </li>
+        <li>
+          <NavLink to="/Rates">Rates</NavLink>
+        </li>
       </ul>
     </nav>
-  )
+  );
 }
+
+export default Nav;
